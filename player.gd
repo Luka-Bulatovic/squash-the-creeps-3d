565ba8f6cdad423cf286f394e6e0a94b6536260d
Luka-Bulatovic <lukab@LUKA-DESKTOP>
2025-07-24 22:56:57 +0200

    Squashing mobs

--- player.gd
+++ player.gd
@@ -6,6 +6,8 @@ extends CharacterBody3D
 @export var fall_acceleration = 75
 # Vertical impulse applied to the character upon jumping in meters per second.
 @export var jump_impulse = 20
+# Vertical impulse applied to the character upon bouncing over a mob in meters per second.
+@export var bounce_impulse = 16
 
 var target_velocity = Vector3.ZERO
 
@@ -47,3 +49,28 @@ func _physics_process(delta):
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+
+	# Iterate through all collisions that occurred this frame
+	# NOTE: This is after move_and_slide() call
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
+
+		# If there are duplicate collisions with a mob in a single frame
+		# the mob will be deleted after the first collision, and a second call to
+		# get_collider will return null, leading to a null pointer when calling
+		# collision.get_collider().is_in_group("mob").
+		# This block of code prevents processing duplicate collisions.
+		if collision.get_collider() == null:
+			continue
+
+		# If the collider is with a mob
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			# we check that we are hitting it from above.
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				# If so, we squash it and bounce.
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				# Prevent further duplicate calls.
+				break
